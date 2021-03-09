af6c65332fceab0978e97dfee7d7ff96aa23bd5d
Willams Abad <willabad97@gmail.com>
2021-03-08 22:15:42 -0500

    Creación de variables para encriptar y descencriptar primary key del rol

--- core/configAPP.php
+++ core/configAPP.php
@@ -4,3 +4,7 @@ const DB = "sistema_mvc";
 const USER ="root";
 const PASS = "";
 const SGBD = "mysql:host=" . SERVER . ";dbname=" . DB;
+
+const SECRET_KEY = "AES-256-CBC";
+const SECRET_IV = "%&&(*()())";
+const METHOD = '101712';
