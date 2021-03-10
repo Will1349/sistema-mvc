d98ab34ebbb12acccfff967390a4c3f8b80bac42
Willams Abad <willabad97@gmail.com>
2021-03-09 23:18:49 -0500

    Se corrige error de escritura en variables "METHOD, SECRET_KEY, SECRET_IV"

--- core/configAPP.php
+++ core/configAPP.php
@@ -5,6 +5,7 @@ const USER ="root";
 const PASS = "";
 const SGBD = "mysql:host=" . SERVER . ";dbname=" . DB;
 
-const SECRET_KEY = "AES-256-CBC";
-const SECRET_IV = '%&&(*()())';
-const METHOD = '101712';
+
+const METHOD = "AES-256-CBC";
+const SECRET_KEY = '$BP@2017';
+const SECRET_IV = '101712';
