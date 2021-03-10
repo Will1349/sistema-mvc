886edf4555d56b4c09fb25836ad861619d43548d
Willams Abad <willabad97@gmail.com>
2021-03-09 23:16:33 -0500

    Se realiza la comparación para insertar o eliminar

--- ajax/rol.ajax.php
+++ ajax/rol.ajax.php
@@ -1,12 +1,22 @@
 <?php
 $peticionAjax = true;
 require_once "../core/configGeneral.php";
-if (isset($_POST['nombre-reg'])) {
+if (isset($_POST['nombre-reg']) || isset($_POST['rolDel'])) {
     require_once "../controlador/rol.controlador.php";
-    $insRol = new RolControlador();
-    echo $insRol->CtrInsertarRol();
-} else {
-    session_start();
-    session_destroy();
-    echo '<script> window.location.href="'.SERVERURL.'login/"</script>';
+    //INSERTAR
+    if (isset($_POST['nombre-reg'])) {
+        require_once "../controlador/rol.controlador.php";
+        $insRol = new RolControlador();
+        echo $insRol->CtrInsertarRol();
+    }
+    if (isset($_POST['rolDel'])) {
+        $delRol = new RolControlador();
+        echo $delRol->CtrEliminarRol();
+    }
 }
+//ELIMINAR
+ else {
+     session_start();
+     session_destroy();
+     echo '<script> window.location.href="'.SERVERURL.'login/"</script>';
+ }
